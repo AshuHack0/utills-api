c02991b984b7360edf3e3a4693ad5124533ca5a0
Ashutosh Kumar <109748201+AshuHack0@users.noreply.github.com>
2024-03-08 21:58:37 +0530

    add scroll effect

--- src/components/components/HiringWithUs.js
+++ src/components/components/HiringWithUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../assets/Welcome.module.css'; // Importing existing CSS module
+import InfiniteScroll from './scroll';
 
 
 
@@ -9,7 +10,8 @@ const HiringWithUs = () => {
     <div className={`container `}>
       <div className="row" style={{ display: 'flex', justifyContent: 'center' , marginTop:'4rem' }} data-aos="fade-up">
         <h1 className={`text-align-center ${styles.heading} mt-4`} style={{textAlign:'center'}}>Companies Hiring with us :</h1>
-        <div className={`col-md-10 d-flex  `} style={{overflow:'hidden' , padding:'2rem'}}>
+        <InfiniteScroll/>
+        {/* <div className={`col-md-10 d-flex  ${styles.animatedBoxMain}`} style={{overflow:'hidden' , padding:'2rem'}}>
           <span className={` ${styles.box} ${styles.animatedBox}`}>
             <img src='https://www.jobhai.com/static/companies/swiggy.png' width={91} height={28}/>
           </span>
@@ -34,18 +36,9 @@ const HiringWithUs = () => {
           <span className={` ${styles.box} ${styles.animatedBox}`}>
             <img src=' https://www.amcham.gr/wp-content/uploads/2015/03/accenture.jpg' width={91} height={28}/>
           </span>
-          <span className={` ${styles.box} ${styles.animatedBox}`}>
-            <img src='https://www.jobhai.com/static/companies/swiggy.png' width={91} height={28}/>
-          </span>
-          <span className={` ${styles.box} ${styles.animatedBox}`}>
-            <img src='https://www.jobhai.com/static/companies/swiggy.png' width={91} height={28}/>
-          </span>
-          <span className={` ${styles.box} ${styles.animatedBox}`}>
-            <img src='https://www.jobhai.com/static/companies/swiggy.png' width={91} height={28}/>
-          </span>
-
           
-        </div>
+          
+        </div> */}
       </div>
     </div>
   );
---
+++ src/components/components/scroll.js
@@ -0,0 +1,92 @@
+// 
+import React, { useEffect, useRef } from 'react';
+import styles from '../assets/scroll.module.css';
+
+const InfiniteScroll = () => {
+  const photoWrapperRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const photos = photoWrapperRef.current.querySelectorAll('.photo');
+      photos.forEach(checkPosition);
+    };
+
+    const checkPosition = (photo) => {
+      if (photo.getBoundingClientRect().right - 4 <= 0) {
+        photo.remove();
+        photoWrapperRef.current.appendChild(photo);
+        photoWrapperRef.current.scrollLeft = 0;
+      }
+    };
+
+ 
+
+const infiniteScroll = () => {
+    const wrapper = photoWrapperRef.current;
+    if (wrapper.scrollLeft >= wrapper.scrollWidth - wrapper.offsetWidth) {
+      wrapper.scrollLeft = 0;
+    } else {
+      wrapper.scrollLeft += 0.5; // Adjust the value to control the scrolling speed
+    }
+    requestAnimationFrame(infiniteScroll);
+  };
+  
+
+    window.addEventListener('scroll', handleScroll);
+    infiniteScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // Empty dependency array ensures that this effect runs only once
+
+  return (
+    <div className={`photoWrapper ${styles.photoWrapper}`} ref={photoWrapperRef}>
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://media.licdn.com/dms/image/D4D12AQHwi4jdRd3fQQ/article-cover_image-shrink_600_2000/0/1685279753620?e=2147483647&v=beta&t=7I-pJ0kDQfNl4w-0Ue8aPyol_X-aWOQlzp18NhTldys" alt="Cat" />
+    </div>
+
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Accenture.svg/2560px-Accenture.svg.png" alt="Cat" />
+    </div>
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Wipro_Primary_Logo_Color_RGB.svg" alt="Cat" />
+    </div>
+    
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://1000logos.net/wp-content/uploads/2021/08/Capgemini-Logo.png" alt="Cat" />
+    </div>
+    
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png" alt="Cat" />
+    </div>  
+    
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Genpact_logo.svg/2560px-Genpact_logo.svg.png" alt="Cat" />
+    </div>  
+
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://mma.prnewswire.com/media/633365/4364085/Infosys_Logo.jpg?p=facebook" alt="Cat" />
+    </div>  
+
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://logos-world.net/wp-content/uploads/2020/05/Facebook-Logo-2019.png" alt="Cat" />
+    </div>  
+
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png" alt="Cat" />
+    </div>
+
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Accenture.svg/2560px-Accenture.svg.png" alt="Cat" />
+    </div>
+    <div className={`photo  ${styles.photo}`}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Wipro_Primary_Logo_Color_RGB.svg" alt="Cat" />
+    </div>
+    
+      {/* Add more photos here */}
+  </div>
+  );
+};
+
+export default InfiniteScroll;
